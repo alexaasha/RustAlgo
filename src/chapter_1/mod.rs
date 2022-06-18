d309b254820eddc43bd70416da12ed5485a00662
Alex <caramboox@gmail.com>
2022-06-18 15:49:18 +0300

    Add tests for binary search

--- src/chapter_1/mod.rs
+++ src/chapter_1/mod.rs
@@ -16,3 +16,40 @@ pub fn simple_binary_search(slice: &[i32], item: i32) -> Option<usize> {
 
     return pos;
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::simple_binary_search;
+
+    #[test]
+    fn odd_amount() {
+        let slice = &[1, 2, 3, 4, 5];
+        let target = 3;
+
+        assert_eq!(simple_binary_search(slice, target).unwrap(), 2);
+    }
+
+    #[test]
+    fn even_amount() {
+        let slice = &[1, 2, 3, 4, 5, 6];
+        let target = 4;
+
+        assert_eq!(simple_binary_search(slice, target).unwrap(), 3);
+    }
+
+    #[test]
+    fn one_element() {
+        let slice = &[1];
+        let target = 1;
+
+        assert_eq!(simple_binary_search(slice, target).unwrap(), 0);
+    }
+
+    #[test]
+    fn no_elements() {
+        let slice = &[];
+        let target = 1;
+
+        assert_eq!(simple_binary_search(slice, target), None);
+    }
+}
