359438c5bedf9ac9c40ee097dc84e726df3fdb15
Alex <caramboox@gmail.com>
2022-06-18 15:35:56 +0300

    Add simple iterative binary search

--- src/chapter_1/mod.rs
+++ src/chapter_1/mod.rs
@@ -1,3 +1,18 @@
-pub fn return2() -> i32 {
-        return 2;
+pub fn simple_binary_search(slice: &[i32], item: i32) -> Option<usize> {
+    let mut begin = 0;
+    let mut end = slice.len();
+
+    let pos = None;
+    while begin < end {
+        let mid = (begin + end) / 2;
+        if slice[mid] == item {
+            return Some(mid);
+        } else if slice[mid] < item {
+            begin = mid + 1;
+        } else {
+            end = mid;
+        }
+    }
+
+    return pos;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 mod chapter_1;
-use chapter_1::return2;
+use chapter_1::simple_binary_search;
 
 fn main() {
-    println!("{}", return2());
+    let slice = &[1, 2, 3, 4, 5];
+    let target = 5;
+
+    println!("{}", simple_binary_search(slice, target).unwrap());
 }
 
